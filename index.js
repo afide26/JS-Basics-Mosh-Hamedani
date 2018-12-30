d088999407f9e65ced1534537061c2ca8f9c9ab2
Alan Fidelino <afidedev26@gmail.com>
2018-12-30 23:42:32 +1100

    Lesson 22 - For of loop for Arrays

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-// For in loops (used for objects)
+//For in loops for objects
 
 let person = {
-  name: "Alan",
+  firstName: 'Alan',
+  lastName: 'Fidelino',
   age: 47,
-  job: 'Developer'
+  job: 'UI/UX and Front End Developer'
 }
 
 for (let key in person)
-  console.log(key, person[key])
+  console.log(`${key}: ${person[key]}`);
+
+
+//For of loops for Arrays
+
+let colors = ['green', 'red', 'blue'];
+
+for (let color of colors)
+  console.log(color);
