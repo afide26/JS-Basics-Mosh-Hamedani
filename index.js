3ca0772f1850e0de53360139352437cc322dddca
Alan Fidelino <afidedev26@gmail.com>
2018-12-30 23:15:58 +1100

    Lesson 18 - For loop

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-//Switch and case
+// For loops
 
-let role = prompt("What is your role?");
+// for (let i = 1; i <= 5; i++) {
+//   console.log('Hello JavaScript!', i)
+// }
 
-switch (role) {
-  case 'Admin':
-    console.log('Admin');
-    break;
-  case 'User':
-    console.log('User');
-    break;
-  default:
-    console.log('Guest');
-}
+// Print out odd numbers by using modulo increment method
+// for (let i = 1; i <= 5; i++)
+//   if (i % 2 !== 0) console.log('Hello World', i)
+
+// Print out odd numbers by using modulo decrement method
+for (let i = 5; i > 0; i--)
+  if (i % 2 !== 0) console.log('Hello Minus', i)
