7ff700f215ff7f0e59d23b8cc80205e418d641ff
Alan Fidelino <afidedev26@gmail.com>
2018-12-30 20:33:19 +1100

    Lesson 13 - Logical Operators with Non-booleans pt1

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-// Ternary Operators
+// Logical Operators with non-booleans
+// Logical AND (&&)
+//Returns true if both sides are true
+//console.log(false && true);// false since one side is false
 
-// If a customer has more than 100 points
-// they are 'gold' customers, otherwise
-// they are 'silver' customers.
+// let highIncome = true;
+// let goodCreditScore = true;
 
-let points = 90;
-let type = points > 100 ? 'Gold' : 'Silver';
+// let eligibleForLoan = highIncome && goodCreditScore;
+// console.log(eligibleForLoan);
 
-console.log(type)
+// LOGICAL OR (||)
+// Either side returns true
+
+let highIncome = true;
+let goodCreditScore = true;
+
+let eligibleForLoan = highIncome || goodCreditScore; //returns true; since one side is true
+console.log('Eligible', eligibleForLoan);
+
+//NOT (!)
+let applicationRefused = !eligibleForLoan;
+console.log('Negated result with !', applicationRefused);
