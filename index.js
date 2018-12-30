bf10dac1ac2c079bf29a797ba08b41a5df74e70e
Alan Fidelino <afidedev26@gmail.com>
2018-12-30 23:26:28 +1100

    Lesson 19 - While loop

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-// For loops
+// While loop
+let i = 0;
 
-// for (let i = 1; i <= 5; i++) {
-//   console.log('Hello JavaScript!', i)
-// }
-
-// Print out odd numbers by using modulo increment method
-// for (let i = 1; i <= 5; i++)
-//   if (i % 2 !== 0) console.log('Hello World', i)
-
-// Print out odd numbers by using modulo decrement method
-for (let i = 5; i > 0; i--)
-  if (i % 2 !== 0) console.log('Hello Minus', i)
+while (i <= 5) {
+  if (i % 2 !== 0) console.log('Hello from While loop too', i);
+  i++;
+}
