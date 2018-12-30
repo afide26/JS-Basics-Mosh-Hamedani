60833bee52580e2a0c1cef59548b93e0b246f06b
Alan Fidelino <afidedev26@gmail.com>
2018-12-31 00:58:09 +1100

    Exercise FizzBuzz

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
-//Exercise Landscape or Portrait
+let title = document.getElementById('title');
+title.innerHTML = "FizzBuzz Algorithm Exercise";
+let style = {
+  fontSize: '30px',
+  fontWeight: 'normal',
+  color: 'red',
+  fontFamily: 'Arial, Helvetica, sans-serif',
+  letterSpacing: '3px'
+}
+Object.assign(title.style, style);
 
-//My Solution
-function isLandscape(width, height) {
-  return (width > height) ? true : false;
+function fizzBuzzMine(number) {
+  if (number % 3 === 0) return console.log("Fizz");
+  if (number % 5 === 0) return console.log("Buzz");
+  if (typeof number !== number) return console.log('NaN')
+  return console.log(number)
 }
 
-// Instructor's solution shorter way
-function isLandscape(width, height) {
-  return (width > height) //this line returns true or false.
+// let fizzNumber = prompt("What is the number?");
+// fizzBuzzMine(fizzNumber);
+
+//Instructor's solution
+
+function fizzBuzz(input) {
+  if (typeof input !== 'number') return console.log(NaN);
+
+  if ((input % 3 === 0) && (input % 5 === 0))
+    return "FizzBuzz";
+  if (input % 3 === 0)
+    return "Fizz";
+  if (input % 5 === 0)
+    return "Buzz"
+  return input
 }
 
-console.log(isLandscape(200, 200))
+
+let p = document.createElement('p');
+p.innerHTML = fizzBuzz(false);
+title.appendChild(p);
+
+console.log(fizzBuzz(false))
