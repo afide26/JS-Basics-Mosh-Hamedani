aa2bf8c4089fe06c46c5a1abb937b4535eff0720
Alan Fidelino <afidedev26@gmail.com>
2018-12-30 19:10:18 +1100

    Lesson 9 - Arithmetic operators

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-function square(num) {
-  return num * num;
-}
+// Variables Expressions Logic
+// Arithmetic
+// Assignment
+// Comparison
+// Logical
+// Bitwise
 
+// Arithmetic Operators
+let x = 10;
+let y = 3;
 
-// let number = square(9);
-// console.log(number);
-console.log(square(3));
+// console.log('Addition-', x + y);
+// console.log('Subtraction-', x - y);
+// console.log('Multiplication-', x * y);
+// console.log('Division-', x / y);
+// console.log('Modulo-', x % y);
+// console.log('Exponentiation-', x ** y);
+
+// Increment Operator
+//console.log(++x); //The value that will show is the mutated value of x right away;
+//console.log(y++); //The console will still log the old value of y until another console log call is made;
+//console.log('New value of y after ++', y);
+
+//Decrement Operator
+console.log(--x); //See above
+console.log(y--); //See above
+console.log('New value of y after -- operator ', y);
