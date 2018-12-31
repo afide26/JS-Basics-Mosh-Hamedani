6bdc83760217a383bb233ecedaf57b33181646d5
Alan Fidelino <afidedev26@gmail.com>
2018-12-31 19:22:55 +1100

    Exercise Add Multiples

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-//Show properties
+//Sums and Multiples of 3 and 5
 
-const person = {
-  name: 'Alan',
-  age: 47,
-  ID: 1234,
-  job: 'Developer'
+console.log(sumMultiples(30))
+
+//3: 3, 6,9,12,15,18,21,24,27,30 =165
+//5: 5, 10,15,20,25,30 = 105
+function sumMultiples(limit) {
+  let multiple = 0;
+  //My solution
+  // for (let i = 0; i <= limit; i++) {
+  //   if ((i % 3 === 0))
+  //     multiple += i;
+  //   else if ((i % 5 === 0))
+  //     multiple += i;
+  // }
+
+  //Instructor's solution
+  for (let i = 0; i <= limit; i++)
+    if (i % 3 === 0 || i % 5 === 0)
+      multiple += i;
+
+  return multiple;
 }
-
-function showProperties(object) {
-  for (let key in object) {
-    if (typeof object[key] === 'string') {
-      console.log(key, object[key])
-    }
-  }
-}
-
-// Call the function
-showProperties(person)
