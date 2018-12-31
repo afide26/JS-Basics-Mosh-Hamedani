eb323923c8378cb627599b674e60da743757d319
Alan Fidelino <afidedev26@gmail.com>
2018-12-31 20:00:29 +1100

    Exercise Show Stars

--- index.js
+++ index.js
@@ -1,47 +1,22 @@
-//Grade
+//Stars
+//My Solution
+showStars(3);
 
-const marks = [99, 75, 99, 77, 100];
+function showStarsMine(rows) {
+  let star = '*'
 
-//0-59: F
-//60-69: D
-//70-79: C
-//80-89: B
-//90-100: A
-
-console.log(calculateGrade(marks))
-
-function calculateGradeOld(marks) {
-  let total = 0;
-
-
-  for (let grade of marks)
-    total += grade
-
-  let average = total / marks.length;
-  console.log(average);
-
-  if (average >= 90) return 'A'
-  else if (average >= 80) return 'B'
-  else if (average >= 70) return 'C'
-  else if (average >= 60) return 'D'
-  else return 'F'
-}
-
-function calculateAverage(arr) {
-  let total = 0;
-
-  for (let value of arr) {
-    total += value
+  for (let i = 0; i <= rows; i++) {
+    console.log(star.repeat(i))
   }
-  return total / arr.length;
 }
 
-function calculateGrade(marks) {
-  const average = Math.round(calculateAverage(marks));
+function showStars(rows) {
 
-  if (average < 60) return `${average} - 'F';`
-  if (average < 70) return `${average} - 'D'`;
-  if (average < 80) return `${average} - 'C'`;
-  if (average < 90) return `${average} - 'B'`;
-  return `${average} - 'A'`
+  for (let row = 0; row <= rows; row++) {
+    let pattern = '';
+    for (let i = 0; i < row; i++) {
+      pattern += '*';
+    }
+    console.log(pattern);
+  }
 }
