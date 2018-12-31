ecfad620a5096517c20739bc0f927f812acb2419
Alan Fidelino <afidedev26@gmail.com>
2018-12-31 20:33:25 +1100

    Exercise Prime Number Refactored

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 //Show Primes
 //Prime (whose factors are one and itself)
 //Composite
-showPrimes(20);
+showPrimes(30);
 
 function showPrimes(limit) {
-  for (let number = 2; number <= limit; number++) {
-    let isPrime = true;
 
-    for (let factor = 2; factor < number; factor++) {
-      if (number % factor === 0) {
-        isPrime = false;
-        break;
-      }
+  for (let number = 2; number <= limit; number++) {
+    if (isPrime(number)) console.log(number)
+  }
+}
+
+function isPrime(number) {
+  for (let factor = 2; factor < number; factor++) {
+    if (number % factor === 0) {
+      return false;
     }
-    if (isPrime) console.log(number);
+    return true;
   }
 }
