c5638611fad489059af1b38f41a3ffb6beb4d46d
Alan Fidelino <afidedev26@gmail.com>
2019-01-01 19:19:35 +1100

    Arrays - Mapping Arrays

--- arrays.js
+++ arrays.js
@@ -1,11 +1,34 @@
-//Lesson Testing the Elements
+//Lesson Mapping an Array
 
 const numbers = [2, -1, 3, 1, 21];
 
 
-//Use filter method - need to pass a callback function
+//Use the Map method
 
 const positiveNumbers = numbers.filter((number) => number >= 0);
-console.log(positiveNumbers);
+const markUps = positiveNumbers.map(n => `<li>${n}</li>`)
+console.log('Mapped', markUps)
 
-// Filtering an array of Objects - normal use case
+//Add the li tags to ul
+
+const html = '<ul>' + markUps.join('') + '</ul>';
+console.log('--------');
+console.log(html)
+
+
+//Mapping to objects
+
+const items = positiveNumbers.map(n => ({
+  value: n
+}));
+console.log(items);
+
+//Chaining
+
+const chained = positiveNumbers.filter(n => n >= 0)
+  .map(n => ({
+    value: n
+  }))
+  .filter(obj => obj.value > 2)
+  .map(obj => obj.value);
+console.log('Chained', chained)
