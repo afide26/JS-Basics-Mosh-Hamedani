1f6181efdb90210e1ba043ba4bcaf1408d9432f1
Alan Fidelino <afidedev26@gmail.com>
2019-01-01 17:09:39 +1100

    Exercise Blog Post

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-//Exercise Factory and Constructor Function
+//Exercise Blog Post Object
 
+//title
+//body
+//author
+//views
+//comments
+//author
+//body
+//isLive
 
-//Constructor Address
-function Address(street, city, zipCode) {
-  this.street = street;
-  this.city = city;
-  this.zipCode = zipCode;
+let blogPost = {
+  title: 'First Blog',
+  body: 'This is the first message in the blog',
+  author: 'John Doe',
+  views: 20,
+  comments: [{
+    author: 'Alan Fidelino',
+    body: 'Hello'
+  }, {
+    author: 'a',
+    body: 'b'
+  }],
+  isLive: true
 }
 
-const add1 = new Address('a', 'b', 1243);
-const add2 = new Address('a', 'b', 1243)
-const add3 = add1;
-
-//Check if the property values are equal
-function areEqual(address1, address2) {
-  return address1.street === address2.street && address1.city === address1.city && address1.zipCode === address2.zipCode;
-}
-
-console.log(areEqual(add1, add2));
-
-//Check if the Object has same reference types
-
-function areSame(address1, address2) {
-  return address1 === address2
-}
-
-console.log(areSame(add1, add2));
-console.log(areSame(add1, add3))
+console.log(blogPost)
