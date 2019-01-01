db4bee283983356002b6b182cc04ae261b86f3ac
Alan Fidelino <afidedev26@gmail.com>
2019-01-01 16:43:19 +1100

    Exercise - Show Address

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-//Section 5
-// 1. Objects
-// 2. Factory Function - naming convention (camelNotation)
-// 3. Constructor Function - naming convention (PascalNotation)
-// 4. Dynamic Nature of Object - you can always add or remove objects
-// 5. Constructor Property
-// 6. Functions are Objects
-// 7. Value vs Reference Types
-// 8. Enumerating Properties of an Object
-// 9. Cloning an Object
-// 10. Garbage collection
-//Memory allocations and dealloactions happen automatically. Memories not used are deallocated automatically.
-//11. Math - built in properties
-//12. String - built in Object
-//13. Template literals - backtick
-//14. Date
+//Exercise Address
+//street
+//city
+// zipcode
 
-const now = new Date();
-const date1 = new Date('May 11 2018 09:00');
-const date2 = new Date(2018, 4, 11, 9, 0)
+let address = {
+  street: 'Marlowe',
+  city: 'Wetherill Pk',
+  zipcode: 2164
+}
+
+function showAddressMine({ ...address
+}) {
+  console.log(`Your address is ${address.street}, ${address.city},
+  ${address.zipcode}`)
+}
+
+function showAddress(address) {
+  //Using for in
+  for (let key in address)
+    console.log(`${key}:`, address[key])
+}
+
+showAddress(address)
