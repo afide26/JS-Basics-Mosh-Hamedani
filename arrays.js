78634d12a996a31af6074255539f92afe71d23e5
Alan Fidelino <afidedev26@gmail.com>
2019-01-01 19:03:13 +1100

    Arrays - Some and Every- Testing Arrays

--- arrays.js
+++ arrays.js
@@ -1,43 +1,13 @@
-//Lesson Sorting Arrays
+//Lesson Testing the Elements
 
-const numbers = [2, 3, 1];
+const numbers = [2, -1, 3, 1, 21];
 
-//Sort - Works only for strings or numbers
-numbers.sort();
-console.log(numbers);
-//Numbers Reverse
-numbers.reverse()
-console.log(numbers)
 
-// Sorting Arrays with objects
-console.log('=========');
+//Use every method
+// const allPositive = numbers.every((value) => value >= 0);
+// console.log('All Positive', allPositive)
 
-const courses = [{
-    id: 1,
-    name: 'Node.js'
-  },
-  {
-    id: 4,
-    name: 'React'
-  },
-  {
-    id: 2,
-    name: 'JavaScript'
-  }
-]
 
-courses.sort();
-console.log(courses); //Nothing will change
-
-//Solution - pass a function to the sort method
-
-courses.sort(function (a, b) {
-  //a < b => -1;
-  //a > b => 1;
-  //a === b => 0;
-  if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
-  if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
-  if (a.name.toLowerCase() === b.name.toLowerCase()) return 0;
-})
-
-console.log(courses);
+//Use some
+const atLeastOnePositive = numbers.some((value) => value >= 0);
+console.log('At least One Positive', atLeastOnePositive)
