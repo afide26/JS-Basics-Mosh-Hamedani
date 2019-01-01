ac4c6bb0d46e8ef8cec43e2d892bf8de241b4591
Alan Fidelino <afidedev26@gmail.com>
2019-01-01 22:42:26 +1100

    Exercise Count Occurences

--- arrays.js
+++ arrays.js
@@ -1,17 +1,23 @@
 //Exercise Moving Elements
-const numbers = [1, 2, 3, 4]
+const numbers = [1, 1, 2, 3, 4, 5, 5]
 
-function move(arr, index, offset) {
-  let position = index + offset;
-  if (position >= arr.length || position < 0) {
-    console.error('Invalid offset');
-    return;
-  }
+function countOccurences(arr, searchElement) {
+  //My Solution
+  // let count = 0;
+  // for (let item of arr) {
+  //   if (item === searchElement) {
+  //     count++;
+  //   }
+  // }
+  // return count;
 
-  const output = [...arr];
-  const element = output.splice(index, 1)[0];
-  output.splice(position, 0, element)
-  return output;
+  //Using reduce method
+
+  return arr.reduce((acc, curr) => {
+    const occurence = (curr === searchElement) ? 1 : 0;
+    return acc + occurence;
+  }, 0)
 }
 
-console.log(move(numbers, 2, 1));
+const count = countOccurences(numbers, 5);
+console.log(count);
