41e503803debc3ad18b56ef794bc58ab3c9dcaee
Alan Fidelino <afidedev26@gmail.com>
2019-01-01 23:06:54 +1100

    Exercise Get Max

--- arrays.js
+++ arrays.js
@@ -1,23 +1,28 @@
-//Exercise Moving Elements
-const numbers = [1, 1, 2, 3, 4, 5, 5]
+//Exercise Get Max
+const numbers = [1, 1, 2, 3, 4, 5, 55]
 
-function countOccurences(arr, searchElement) {
-  //My Solution
-  // let count = 0;
-  // for (let item of arr) {
-  //   if (item === searchElement) {
-  //     count++;
-  //   }
-  // }
-  // return count;
-
-  //Using reduce method
-
-  return arr.reduce((acc, curr) => {
-    const occurence = (curr === searchElement) ? 1 : 0;
-    return acc + occurence;
-  }, 0)
+function getMaxMine(array) {
+  return Math.max(...array)
 }
 
-const count = countOccurences(numbers, 5);
-console.log(count);
+//Instructor's solution
+function getMax(array) {
+  //Simple solution
+  // if (array.length === 0) return undefined;
+
+  // let max = array[0];
+  // for (let i = 1; i < array.length; i++)
+  //   max = array[i];
+  // return max;
+
+  if (array.length === 0) return undefined;
+
+  return array.reduce((a, b) => {
+    return a > b ? a : b;
+  })
+
+
+}
+
+console.log(getMax([]));
+// Solution from https://www.jstips.co/en/javascript/calculate-the-max-min-value-from-an-array/
