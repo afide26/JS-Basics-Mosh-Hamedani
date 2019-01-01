a5d66d40f0e8554200acccf7f6026acea6094500
Alan Fidelino <afidedev26@gmail.com>
2019-01-01 19:27:51 +1100

    Arrays - Reduce method

--- arrays.js
+++ arrays.js
@@ -1,34 +1,19 @@
-//Lesson Mapping an Array
+//Lesson Reducing an Array
 
 const numbers = [2, -1, 3, 1, 21];
 
 
-//Use the Map method
+//Use Long method
 
-const positiveNumbers = numbers.filter((number) => number >= 0);
-const markUps = positiveNumbers.map(n => `<li>${n}</li>`)
-console.log('Mapped', markUps)
+let sum = 0;
 
-//Add the li tags to ul
+for (let num of numbers)
+  sum += num;
+console.log(sum);
 
-const html = '<ul>' + markUps.join('') + '</ul>';
-console.log('--------');
-console.log(html)
+//More elegant => Reduce method 2 arguments, function, and initial value;
 
-
-//Mapping to objects
-
-const items = positiveNumbers.map(n => ({
-  value: n
-}));
-console.log(items);
-
-//Chaining
-
-const chained = positiveNumbers.filter(n => n >= 0)
-  .map(n => ({
-    value: n
-  }))
-  .filter(obj => obj.value > 2)
-  .map(obj => obj.value);
-console.log('Chained', chained)
+const reducedSum = numbers.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+}, 0);
+console.log(reducedSum);
