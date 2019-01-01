8a4cb22dcca36a67d4afc4ab76c15e03ff58ad0d
Alan Fidelino <afidedev26@gmail.com>
2019-01-01 18:58:57 +1100

    Arrays - Sorting

--- arrays.js
+++ arrays.js
@@ -1,14 +1,43 @@
-//Lesson Joining Arrays
+//Lesson Sorting Arrays
 
-const numbers = [1, 2, 3];
+const numbers = [2, 3, 1];
 
-//Joining
-const joined = numbers.join(',') //returns a string, not an array
-console.log(joined);
+//Sort - Works only for strings or numbers
+numbers.sort();
+console.log(numbers);
+//Numbers Reverse
+numbers.reverse()
+console.log(numbers)
 
-//Split
-const message = 'This is my first message'
-const parts = message.split(' ') //returns an array an not a string;
-console.log(parts);
-const joinedTwo = parts.join('-') //returns a string, not an array
-console.log(joinedTwo)
+// Sorting Arrays with objects
+console.log('=========');
+
+const courses = [{
+    id: 1,
+    name: 'Node.js'
+  },
+  {
+    id: 4,
+    name: 'React'
+  },
+  {
+    id: 2,
+    name: 'JavaScript'
+  }
+]
+
+courses.sort();
+console.log(courses); //Nothing will change
+
+//Solution - pass a function to the sort method
+
+courses.sort(function (a, b) {
+  //a < b => -1;
+  //a > b => 1;
+  //a === b => 0;
+  if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+  if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
+  if (a.name.toLowerCase() === b.name.toLowerCase()) return 0;
+})
+
+console.log(courses);
