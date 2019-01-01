7584fe42e9cc822142e1eb47eb455594eab6026d
Alan Fidelino <afidedev26@gmail.com>
2019-01-01 16:36:35 +1100

    Objects - Date

--- index.js
+++ index.js
@@ -13,12 +13,8 @@
 //11. Math - built in properties
 //12. String - built in Object
 //13. Template literals - backtick
+//14. Date
 
-const message = `This is my
-'first' message`;
-console.log(message);
-
-if (message.includes('first')) {
-  console.log(message.replace('first', 'second'))
-}
-console.log(message);
+const now = new Date();
+const date1 = new Date('May 11 2018 09:00');
+const date2 = new Date(2018, 4, 11, 9, 0)
