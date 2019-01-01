a7b630535cd9d27ab52bf96841c6dabcf0ecac57
Alan Fidelino <afidedev26@gmail.com>
2019-01-01 17:17:17 +1100

    Exercise Blog Post - Constructor Function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Exercise Blog Post Object
+//Exercise Blog Post Object - Constructoru
 
 //title
 //body
@@ -9,19 +9,32 @@
 //body
 //isLive
 
-let blogPost = {
-  title: 'First Blog',
-  body: 'This is the first message in the blog',
-  author: 'John Doe',
-  views: 20,
-  comments: [{
-    author: 'Alan Fidelino',
-    body: 'Hello'
-  }, {
-    author: 'a',
-    body: 'b'
-  }],
-  isLive: true
+// let blogPost = {
+//   title: 'First Blog',
+//   body: 'This is the first message in the blog',
+//   author: 'John Doe',
+//   views: 20,
+//   comments: [{
+//     author: 'Alan Fidelino',
+//     body: 'Hello'
+//   }, {
+//     author: 'a',
+//     body: 'b'
+//   }],
+//   isLive: true
+// }
+
+// console.log(blogPost);
+
+function Post(title, body, author) {
+  this.title = title;
+  this.body = body;
+  this.author = author
+  this.views = 0;
+  this.comments = [];
+  this.isLive = false
 }
 
-console.log(blogPost)
+let newPost = new Post('New Blog', 'Test', 'Alan');
+
+console.log(newPost)
