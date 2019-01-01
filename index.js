3295f5a7d7ddc01f54751da92ee1ecbcaec86475
Alan Fidelino <afidedev26@gmail.com>
2019-01-01 15:32:39 +1100

    Objects - Constructor property

--- index.js
+++ index.js
@@ -3,16 +3,31 @@
 // 2. Factory Function - naming convention (camelNotation)
 // 3. Constructor Function - naming convention (PascalNotation)
 // 4. Dynamic Nature of Object - you can always add or remove objects
+// 5. Constructor Property
 
-const circle = {
-  radius: 1
+//Factory Function
+function createCircle(radius) {
+  return {
+    radius,
+    draw() {
+      console.log('Factory Draw')
+    },
+    color: 'red'
+  }
 }
 
-circle.color = 'yellow'
-circle.draw = function () {
-  console.log('Draw')
+const circle1 = createCircle(3);
+
+// Constructor Function
+
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function () {
+    console.log('Constructor Draw')
+  }
 }
 
-delete(circle.color)
-delete circle.draw
-console.log(circle);
+const circle2 = new Circle(4);
+
+//Take aways
+//Every object has a constructor property that points to the function that created the object
