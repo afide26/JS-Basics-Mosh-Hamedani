d4071306118f62ce40ade19ea3e6d28cc419c335
Alan Fidelino <afidedev26@gmail.com>
2019-01-02 18:33:35 +1100

    Exercise - Area of a Circle

--- functions.js
+++ functions.js
@@ -1,24 +1,19 @@
-//Sum of Arguments
+//Exercise - Areas of a circle;
 
-function sumMine(...args) {
-  if (Array.isArray(args) === true) {
-    args.reduce((a, b) => a + b)
+const circleMine = {
+  radius: 2,
+  area: 20,
+  get circleArea() {
+    return this.area
   }
-  let total = 0;
-  for (let value of args)
-    total += value;
-  return total;
-
 }
 
-//Instructor's solution
-function sum(...items) {
-  if (items.length === 1 && Array.isArray(items[0]))
-    items = [...items[0]];
-  return items.reduce((a, b) => a + b)
+//Instructor's solution with formula for circle's area
+const circle = {
+  radius: 2,
+  get area() {
+    return Math.PI * this.radius * this.radius;
+  }
 }
 
-let numbers = [1, 2, 3, 5]
-
-console.log(sum(1, 2, 3, 4));
-console.log(sum(numbers))
+console.log(circle.area)
