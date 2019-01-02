6cdfe43cc0a8aa58bee5986d033cec5e6c9af85e
Alan Fidelino <afidedev26@gmail.com>
2019-01-02 16:10:02 +1100

    Lesson 2 - Hoisting

--- functions.js
+++ functions.js
@@ -1,14 +1,17 @@
-//Lesson 1
+//Lesson 2 - Hoisting
 //Function Declaration - named functions
+walk(); //can be called before it's defined;
+//All function declarations are moved to the top of the file. Hoisting
+run(); // function expressions cannot be called before it's defined as the variable is not yet defined before calling the function.
 
 function walk() {
   console.log('Walk');
 }
 
 //Function expression - anonymous function assigned to a variable
-let run = function () {
+const run = function () {
   console.log('Run')
 };
 
-walk();
+
 run();
