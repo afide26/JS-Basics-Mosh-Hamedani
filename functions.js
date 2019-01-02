32161d60fb8086df5aa4378eae70d913afc025ba
Alan Fidelino <afidedev26@gmail.com>
2019-01-02 16:41:37 +1100

    Lesson 5 - Default Parameters

--- functions.js
+++ functions.js
@@ -1,27 +1,21 @@
-//Lesson 3 - Rest Operator
+//Lesson 4 - Default Parameters
 
-//Dynamic language - arguments of a function
+//Default parameters
 
+//Use case:  default interest using logical or
+function interest(principal, rate, years) {
+  //Use the logical or to use a default value if no argument is provided
+  rate = rate || 3.5;
+  years = years || 5;
 
-//Make the code flexible to accept multiple arguments
-
-function add(...args) { //rest operator with arguments ...args
-  return args.reduce((a, b) => a + b);
+  return principal * rate / 100 * years;
 }
 
-console.log(add(1, 3, 4, 56, 19));
-
-//Real use case: Shopping cart with discount
-//Check how the discount was calculated (1 - discount);
-
-function checkDiscount(discount, ...prices) { //...prices is same as arguments and the last parameter of the function
-  const total = prices.reduce((a, b) => a + b);
-  const totalDiscount = total - (total * (1 - discount));
-  return `Total Price is: $${total * (1-discount)}\n Total discount is: $${totalDiscount}`
+//Use case:  using default params
+function interestParams(principal, rate = 3.5, years = 5) {
+  //Need to pass default values after the first param or use undefined if not using a predefined value;
+  return principal * rate / 100 * years;
 }
 
-let rice = 20;
-let milk = 40;
-let disc = 0.2;
 
-console.log(checkDiscount(disc, milk, rice))
+console.log(interestParams(10000));
