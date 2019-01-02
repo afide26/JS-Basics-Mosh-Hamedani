6ad5fa01cd115a431ae7b4b552c7d1d012915768
Alan Fidelino <afidedev26@gmail.com>
2019-01-02 17:07:26 +1100

    Lesson 7 - Error Handling

--- functions.js
+++ functions.js
@@ -1,24 +1,41 @@
-//Lesson 5 Getters and Setters
+//Lesson 7 - Error Handling
+
 const person = {
   firstName: 'Alan',
   lastName: 'Fidelino',
-  personFullName() { //Read only
-    return `${person.firstName} ${person.lastName}`
-  },
-  get fullName() { //Prefix the function with get
-    return `${person.firstName} ${person.lastName}`
-  },
-  set fullName(value) { //Prefix the function with get
-    const parts = value.split(' ');
+  set fullName(value) {
+    //Error handling in the first line of the code block; defensive programming.
+    // if (typeof value !== 'string') return console.error(`The value must be a string`)
+    //Throwing an error or throw an exception. When the unexpected happens
+    if (typeof value !== 'string') {
+      throw new Error('The value must be a string');
+    }
+
+    const parts = value.split(' '); // convert the passed string to an array;
+    if (parts.length !== 2) {
+      throw new Error('Enter a first and last name!')
+    }
     this.firstName = parts[0];
     this.lastName = parts[1];
+  },
+
+  get fullName() {
+    return `${person.firstName} ${person.lastName}`;
   }
 }
 
-person.fullName = "Carl Fidelino"
-console.log(person)
-console.log(person.personFullName()); //Read only
-console.log(person.fullName); //Getter, see the property was accessed.
+//person.fullName = 'Carl Fidelino'; //Setter
 
-//getters - access properties of an object
-//setters - to change or mutate the values of properties of an object
+//use the try and catch
+
+try {
+  //try a statement for errors
+  person.fullName = 'Alan Fidelino';
+} catch (e) {
+  alert(e);
+}
+console.log(person) //Getter;
+
+//*** Take aways: */
+// When an exception is thrown, the next lines are not executed, exceptions can be done for multiple cases;
+//Try and catch to see if there are errors in the code execution;
