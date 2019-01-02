cd86882871274d82e0459d27c780834140078873
Alan Fidelino <afidedev26@gmail.com>
2019-01-02 16:06:48 +1100

    Lesson 1 - Function Expression vs Function Declaration

---
+++ functions.js
@@ -0,0 +1,14 @@
+//Lesson 1
+//Function Declaration - named functions
+
+function walk() {
+  console.log('Walk');
+}
+
+//Function expression - anonymous function assigned to a variable
+let run = function () {
+  console.log('Run')
+};
+
+walk();
+run();
