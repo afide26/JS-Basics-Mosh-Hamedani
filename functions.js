6b1e22fce0306012de19913e5fcde6bd7afd21cf
Alan Fidelino <afidedev26@gmail.com>
2019-01-02 17:42:12 +1100

    Lesson 10 - The This keyword

--- functions.js
+++ functions.js
@@ -1,20 +1,31 @@
-//Lesson 9 - Let vs Var
-//Difference with var vs let
-//Problems with var
-//var is function scoped
-//const and let are block scoped 
-function starVar() {
-  for (var i = 0; i < 5; i++)
-    console.log(i)
-  console.log(`i is still accessible outside the for loop ${i}`);
+//Lesson 10 - The This keyboard
+
+// this refers to object that is executing the current function
+
+// function in an object is a method -> this refers to the object
+// regular function -> this refers to global(window, global(in Node))
+
+//Example:
+const video = {
+  title: 'a',
+  play() {
+    console.log('Play', this)
+  }
+}
+video.stop = function () {
+  console.log('Stop', this)
+};
+//video.stop(); // will print the entire video object;
+
+function playVideo() {
+  console.log(this);
 }
 
-starVar();
+//playVideo();
 
-function start() {
-  for (let i = 0; i < 5; i++)
-    console.log(i)
-  console.error(`i is not accessible outside the for loop ${i}`);
+function Video(title) {
+  this.title = title;
+  console.log(this);
 }
 
-start();
+const v = new Video('A')
