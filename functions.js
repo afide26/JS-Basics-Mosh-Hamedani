0d7378965e84ee6bc1eb511c52567462856c5c15
Alan Fidelino <afidedev26@gmail.com>
2019-01-02 18:44:28 +1100

    Exercise - Error Handling! Course Finished

--- functions.js
+++ functions.js
@@ -1,19 +1,21 @@
-//Exercise - Areas of a circle;
+//Exercise - Error Handling
 
-const circleMine = {
-  radius: 2,
-  area: 20,
-  get circleArea() {
-    return this.area
-  }
+
+
+
+function countOccurrences(array, searchElement) {
+  if (!Array.isArray(array))
+    throw new Error('The argument must be an array');
+
+  return array.reduce((a, b) => {
+    const occurence = (b === searchElement) ? 1 : 0;
+    return a + occurence;
+  }, 0)
 }
-
-//Instructor's solution with formula for circle's area
-const circle = {
-  radius: 2,
-  get area() {
-    return Math.PI * this.radius * this.radius;
-  }
+try {
+  const numbers = [1, 2, 3, 1, 4, 1];
+  const count = countOccurrences(false, 1);
+  console.log(count);
+} catch (e) {
+  console.log(e.message)
 }
-
-console.log(circle.area)
